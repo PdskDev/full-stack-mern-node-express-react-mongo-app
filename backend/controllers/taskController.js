3c2da6e83a8cbc07c49c4cd47ce5599b82677b05
NadetDev <nadetdev@gmail.com>
2024-01-30 21:13:09 +0100

    add dummy list of tasks

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,6 +1,35 @@
+const tasks = [
+  {
+    id: 5,
+    title: 'Learn Java',
+    isDone: true,
+  },
+  {
+    id: 6,
+    title: 'Learn PHP',
+    isDone: false,
+  },
+  {
+    id: 7,
+    title: 'Visit Paris',
+    isDone: true,
+  },
+  {
+    id: 8,
+    title: 'Attend team meeting',
+    isDone: false,
+  },
+  {
+    id: 9,
+    title: 'Delivery my app via Jenkins',
+    isDone: true,
+  },
+];
+
 const getTasks = (req, res) => {
   res.status(200).json({
-    message: 'Lecture de toutes les tâches',
+    message: 'list of tasks',
+    tasks: tasks,
   });
 };
 
