d77ce56aa83973a8c0e343876fdcbb9588beed86
NadetDev <nadetdev@gmail.com>
2024-01-30 20:41:58 +0100

    Dummy routes for tests

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,32 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
-//Get all tasks
+//get all tasks
 router.get('/', (req, res) => {
   res.status(200).json({
     message: 'Lecture de toutes les tâches',
   });
 });
 
-//Get specific task
+//get specific task
 router.get('/:taskId', (req, res) => {
   res.status(200).json({
     message: `The id of task is ${req.params.taskId}`,
   });
 });
 
+//create new task
 router.post('/', (req, res) => {
   res.status(200).json({
-    message: `Task is created with id ${Math.random(5).toString}`,
+    message: `Task is created with id ${Math.floor(Math.random() * 99) + 1}`,
   });
 });
 
+//update task
 router.put('/:taskId', (req, res) => {
   res.status(200).json({
     message: `Task ${req.params.taskId} is updated successfully`,
   });
 });
 
+//delete task
 router.delete('/:taskId', (req, res) => {
   res.status(200).json({
     message: `Task ${req.params.taskId} is deleted successfully`,
