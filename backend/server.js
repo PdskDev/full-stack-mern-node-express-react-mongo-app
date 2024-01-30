03ad7ffab978cefd74cfa281418ae13395ef5449
NadetDev <nadetdev@gmail.com>
2024-01-30 19:28:05 +0100

    Initial server & route

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(port, () => {
+  console.log(`Server started at port ${port}`);
+});
+
+app.get('/api/tasks', (req, res) => {
+  res.send('Welcome to our Node Tasks API');
+});
