94eb72dcf3ee23cd9403e5f633ae9e960691043c
NadetDev <nadetdev@gmail.com>
2024-01-30 23:44:39 +0100

    MongoDB CRUD done

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -3,36 +3,68 @@ const Task = require('../models/taskModel');
 
 const getTasks = asyncHandler(async (req, res) => {
   const tasks = await Task.find();
-  res.status(200).json(tasks);
-});
-
-const getTaskById = asyncHandler((req, res) => {
   res.status(200).json({
-    message: `The id of task is ${req.params.taskId}`,
+    count: tasks.length,
+    tasks: tasks,
   });
 });
 
-const createTask = asyncHandler((req, res) => {
+const getTaskById = asyncHandler(async (req, res) => {
+  const existTask = await Task.findById(req.params.taskId);
+  if (!existTask) {
+    res.status(400);
+    throw new Error('Task not found');
+  }
+  res.status(200).json(existTask);
+});
+
+const createTask = asyncHandler(async (req, res) => {
   console.log(req.body);
+
+  const taskIsDone = req.body.isDone ? req.body.isDone : false;
 
   if (!req.body.title) {
     res.status(400);
     throw new Error('Please enter task title');
   }
-  res.status(200).json({
-    message: `Task is created with id ${Math.floor(Math.random() * 99) + 1}`,
+
+  const newTask = await Task.create({
+    title: req.body.title,
+    isDone: taskIsDone,
   });
+  res.status(200).json(newTask);
 });
 
-const updateTask = asyncHandler((req, res) => {
-  res.status(200).json({
-    message: `Task ${req.params.taskId} is updated successfully`,
-  });
+const updateTask = asyncHandler(async (req, res) => {
+  if (!req.body.title) {
+    res.status(400);
+    throw new Error('Please enter task title');
+  }
+
+  const existTask = await Task.findById(req.params.taskId);
+  if (!existTask) {
+    res.status(400);
+    throw new Error('Task not found');
+  }
+
+  const updatedTask = await Task.findByIdAndUpdate(
+    req.params.taskId,
+    req.body,
+    { new: true }
+  );
+  res.status(200).json(updatedTask);
 });
 
-const deleteTask = asyncHandler((req, res) => {
+const deleteTask = asyncHandler(async (req, res) => {
+  const taskToDelete = await Task.findById(req.params.taskId);
+  if (!taskToDelete) {
+    res.status(400);
+    throw new Error('Task not found');
+  }
+
+  await Task.findByIdAndDelete(req.params.taskId);
   res.status(200).json({
-    message: `Task ${req.params.taskId} is deleted successfully`,
+    message: `Task with id ${req.params.taskId} was deleted successfully`,
   });
 });
 
--- backend/models/taskModel.js
+++ backend/models/taskModel.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const taskSchema = mongoose.Schema(
   {
     title: { type: String, required: [true, 'Please add a title value'] },
+    isDone: { type: Boolean, required: false, default: false },
   },
   {
     timestamps: true,
