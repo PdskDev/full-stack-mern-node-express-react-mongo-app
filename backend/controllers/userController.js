f6a3185a0bb253e28f3a5cb05a4e473aa0c2eb4a
NadetDev <nadetdev@gmail.com>
2024-01-31 19:33:10 +0100

    User CRUD: create/register

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,36 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.json({ message: 'Get user register' });
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('All fields are required');
+  }
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const newUser = await User.create({ name, email, password: hashedPassword });
+
+  if (newUser) {
+    res.status(201).json({
+      message: 'User created successfully',
+      user: { _id: newUser.id, name: newUser.name, email: newUser.email },
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
 });
 
 const loginUser = asyncHandler(async (req, res) => {
