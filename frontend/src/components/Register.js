e544aca993eea92c92e46b08bcf0fb09642de65c
NadetDev <nadetdev@gmail.com>
2024-02-02 21:32:44 +0100

    Register page

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,7 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { FaUser } from 'react-icons/fa';
 
 const Register = () => {
-  return <div>Register page</div>;
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+  const { name, email, password, confirmPassword } = formData;
+
+  const onChangeHandler = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <section className='heading'>
+        <h1>
+          <FaUser />
+          Register
+        </h1>
+        <p>Please create an account</p>
+      </section>
+      <section className='form'>
+        <form onSubmit={onSubmitHandler}>
+          <div className='form-group'>
+            <input
+              type='text'
+              className='form-control'
+              id='name'
+              name='name'
+              value={name}
+              placeholder='Enter your name'
+              onChange={onChangeHandler}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='text'
+              className='form-control'
+              id='email'
+              name='email'
+              value={email}
+              placeholder='Enter your email'
+              onChange={onChangeHandler}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              name='password'
+              value={password}
+              placeholder='Enter your password'
+              onChange={onChangeHandler}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='confirmPassword'
+              name='confirmPassword'
+              value={confirmPassword}
+              placeholder='Please confirm your password'
+              onChange={onChangeHandler}
+            />
+          </div>
+          <div className='form-group'>
+            <button type='submit' className='btn btn-block' id='submitButton'>
+              Register
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 };
 
 export default Register;
