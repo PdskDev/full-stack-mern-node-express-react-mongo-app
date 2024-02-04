244cfb8e24b21d3c622f2a09cc4694aff8863f68
NadetDev <nadetdev@gmail.com>
2024-02-04 18:10:17 +0100

    Add logout handler

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'api/users/';
+const API_URL = '/api/users';
 const register = async (userData) => {
   const response = await axios.post(API_URL, userData);
 
@@ -10,5 +10,7 @@ const register = async (userData) => {
   return response.data;
 };
 
-const authService = { register };
+const logout = () => localStorage.removeItem('taskUser');
+
+const authService = { register, logout };
 export default authService;
